e4428a34343c6817db99774a7c21f8f7376278ff
sagnikcapital <sagnikd@capitalnumbers.com>
2024-03-15 12:31:24 +0530

    qr-code

--- src/Components/Layout/Modal/Details.js
+++ src/Components/Layout/Modal/Details.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Button, Modal } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import ReactDOM from "react-dom";
+import QRCode from "react-qr-code";
 
 function Details({ open, onClose, data }) {
   return (
@@ -19,6 +21,14 @@ function Details({ open, onClose, data }) {
           </div>
         )}
         </Modal.Body>
+        <div style={{ height: "auto", margin: "0 auto", maxWidth: 64, width: "100%" }}>
+          <QRCode
+            size={256}
+            style={{ height: "auto", maxWidth: "100%", width: "100%" }}
+            value={data.sys.country}
+            viewBox={`0 0 256 256`}
+          />
+        </div>
         <Modal.Footer>
           <Button variant="secondary" onClick={onClose}>
             Close
