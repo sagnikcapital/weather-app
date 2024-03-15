a7dea25475783ec29cf6168592871bfd58006253
sagnikcapital <sagnikd@capitalnumbers.com>
2024-03-15 18:18:43 +0530

    qr-code

--- src/Components/ContactUs/index.js
+++ src/Components/ContactUs/index.js
@@ -1,44 +1,38 @@
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from '../Layout/Header/Header';
+import { Container, Row, Col, Form, Button } from 'react-bootstrap';
 
 function ContactUs() {
   return (
     <>
+      <Header></Header>
       <section className="section">
-        <div className="container">
-          <h1 className="title">Please contact Us</h1>
-          <div className="columns">
-            <div className="column is-half">
-              <form action="https://fabform.io/f/{insert-your-form-id}" method="post">
-                <div className="field">
-                  <label className="label">Name</label>
-                  <div className="control">
-                    <input className="input" type="text" placeholder="Your Name" />
-                  </div>
-                </div>
-                <div className="field">
-                  <label className="label">Email</label>
-                  <div className="control">
-                    <input className="input" type="email" placeholder="Your Email" />
-                  </div>
-                </div>
-                <div className="field">
-                  <label className="label">Message</label>
-                  <div className="control">
-                    <textarea className="textarea" placeholder="Your Message"></textarea>
-                  </div>
-                </div>
-                <div className="field">
-                  <div className="control">
-                    <button className="button is-primary">Submit</button>
-                    <p>Powered by <a href="https://fabform.io" target="_blank">fabform.io</a></p>
-                  </div>
-                </div>
-              </form>
-            </div>
-          </div>
-        </div>
-      </section>
+      <Container>
+        <h1 className="title">Please contact Us</h1>
+        <Row>
+          <Col lg={6}>
+            <Form action="#" method="post">
+              <Form.Group controlId="formName">
+                <Form.Label>Name</Form.Label>
+                <Form.Control type="text" placeholder="Your Name" />
+              </Form.Group>
+              <Form.Group controlId="formEmail">
+                <Form.Label>Email</Form.Label>
+                <Form.Control type="email" placeholder="Your Email" />
+              </Form.Group>
+              <Form.Group controlId="formMessage">
+                <Form.Label>Message</Form.Label>
+                <Form.Control as="textarea" placeholder="Your Message" />
+              </Form.Group>
+              <Button variant="primary" type="submit">
+                Submit
+              </Button>
+            </Form>
+          </Col>
+        </Row>
+      </Container>
+    </section>
     </>
   );
 }
